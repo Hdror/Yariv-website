f29a4a932410ae0adb8237d976bc3757c8b2629f
Dror Handler <hdror507@gmail.com>
2023-02-02 17:54:08 +0200

    clearing unused files

--- src/cmps/carousel.jsx
+++ src/cmps/carousel.jsx
@@ -5,12 +5,12 @@ import { Paper, Button } from '@mui/material'
 export function ImgCarousel() {
 
     const items = [
-        <img style={{ height: "600px", objectFit: "cover" }} src="http://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379708/link-imgs/%D7%9E%D7%90%D7%99_%D7%95%D7%99%D7%90%D7%99%D7%A8-155_rslqdw.jpg" alt='' />,
-        <img style={{ height: "600px", objectFit: "cover" }} src="https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379204/carousel-imgs/%D7%93%D7%99%D7%9F_%D7%95%D7%90%D7%91%D7%99%D7%91_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-143_ui1gck.jpg" alt='' />,
-        <img style={{ height: "600px", objectFit: "cover" }} src="https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379186/carousel-imgs/%D7%A1%D7%91%D7%99%D7%95%D7%9F_%D7%95%D7%98%D7%9C-200_t2vshg.jpg" alt='' />,
-        <img style={{ height: "600px", objectFit: "cover" }} src="https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%A6%D7%99%D7%9C%D7%95%D7%9E%D7%99_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-16_t0fnj3.jpg" alt='' />,
-        <img style={{ height: "600px", objectFit: "cover" }} src="https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%9E%D7%90%D7%99_%D7%95%D7%92%D7%99%D7%9017_fo7rff.jpg" alt='' />,
-        <img style={{ height: "600px", objectFit: "cover" }} src="https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%93%D7%99%D7%9F_%D7%95%D7%90%D7%91%D7%99%D7%91_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-69_tw3kzz.jpg" alt='' />,
+         "http://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379708/link-imgs/%D7%9E%D7%90%D7%99_%D7%95%D7%99%D7%90%D7%99%D7%A8-155_rslqdw.jpg" ,
+         "https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379204/carousel-imgs/%D7%93%D7%99%D7%9F_%D7%95%D7%90%D7%91%D7%99%D7%91_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-143_ui1gck.jpg" ,
+         "https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379186/carousel-imgs/%D7%A1%D7%91%D7%99%D7%95%D7%9F_%D7%95%D7%98%D7%9C-200_t2vshg.jpg",
+         "https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%A6%D7%99%D7%9C%D7%95%D7%9E%D7%99_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-16_t0fnj3.jpg",
+         "https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%9E%D7%90%D7%99_%D7%95%D7%92%D7%99%D7%9017_fo7rff.jpg",
+         "https://res.cloudinary.com/dqihtdtoa/image/upload/c_crop,g_auto:faces,h_3000,w_6000/v1673379185/carousel-imgs/%D7%93%D7%99%D7%9F_%D7%95%D7%90%D7%91%D7%99%D7%91_%D7%94%D7%96%D7%95%D7%92%D7%99%D7%95%D7%AA-69_tw3kzz.jpg",
     ]
 
     return (
@@ -26,7 +26,7 @@ export function ImgCarousel() {
 function Item(props) {
     return (
         <Paper>
-            {props.item}
+            <img style={{ height: "600px", objectFit: "cover" }} src={props.item} alt="wedding" />
         </Paper>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './assets/styles/styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RootCmp } from './root-cmp';
 import { Provider } from 'react-redux';
 import { store } from './store/store'
-import './assets/styles/styles.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
